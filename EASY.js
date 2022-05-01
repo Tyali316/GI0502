686246e3e1aa0ca7c0fa65c64793c3290ed6271b
Tyali Warren <tyaliwarren@MacBook-Pro-3.lan>
2022-04-30 22:35:21 -0400

    EASY

--- EASY.js
+++ EASY.js
@@ -6,7 +6,7 @@
 // console.log(swim()); // prints "Today's exercise: swimming"
 
 function exercise(x) {
-    // I nested the functions because with the trial and error, the var run only works this way//
+    // I nested the functions because with trial and error, the var run only works this way//
     function exercising(){
 return `Today's exercise: ${x}`;
     }
