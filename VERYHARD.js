d7c2b88d0f42a6c26548753615bebd6132f89f77
Tyali Warren <tyaliwarren@MacBook-Pro-3.lan>
2022-05-02 23:00:10 -0400

    VERYHARD

--- VERYHARD.js
+++ VERYHARD.js
@@ -5,52 +5,45 @@
 
 // 1. Create a Person constructor that has three properties: name, job, and age.
 
-function Person(name, job, age) {
-    this.name = name;
-    this.job = job;
-    this.age = age;
-}
-const me = new Person ("Tyali Warren", "Software Engineer", "25");
-
-console.log(me);
-
+class Person {
+    constructor(name, job, age) {
+        this.name = name;
+        this.job = job;
+        this.age = age;
+    }
+    
 
 
 // 2. Give the Person an 'exercise' method that console logs whatever you want, e.g. "Running is
 // fun! - said no one ever".
 
-// function Person(name, job, age) {
-//     this.name = name;
-//     this.job = job;
-//     this.age = age;
-//     this.exercise = function(){ // added exercise as a method//
-//         console.log(`Running is okay.`);
-//     }
-// }
-// const p1 = new Person ("Tyali Warren", "Software Engineer", "25");
-// p1.exercise(); 
-
+exercise(){
+    console.log(`Running is okay.`);
+}
 
 // 3. Give the Person a 'fetchJob' method that console logs the person's name and job, e.g. "Brad
 // is a back-end developer".
 
-function Person(name, job, age) {
-    this.name = name;
-    this.job = job;
-    this.age = age;
-    this.fetchJob = function(){
-        console.log(`This is ${this.name} and my job is ${this.job}`);
-    }
-  }
-  
-  const p2 = new Person ("Dwight Scrute", "Back-end Developer", "42");
-  p2.fetchJob();
-
-
+fetchJob() {
+    console.log(`This is ${this.name} and my job is ${this.job}`);
+}
+}  
+let p1 = new Person("Tyali Warren", "Back-End Developer", 25);
+p1.exercise();
+p1.fetchJob();
 
 // 4. Create a Programmer constructor that inherits all the members from Person with an additional
 // 'languages' property that is passed in and a busy property that is NOT passed in and is set to
 // true by default.
+
+class Programmer {
+    constructor(name, job, age, language){
+        this.name = name;
+        this.job = job;
+        this.age = age;
+        this.language = language;
+        this.busy = true;
+    }
 
 
 
@@ -58,14 +51,26 @@ function Person(name, job, age) {
 // programmer to be false. Also give the Programmer an 'acceptNewTask' method that updates the
 // busy property on that programmer to be true.
 
-
+completeTask(){
+    return this.busy = false;
+}
+acceptNewTask(){
+    return this.busy = true;
+}
 
 // 6. Give the Programmer an 'offerNewTask' method that console logs one thing if the
 // programmer is busy and another if the programmer is not, e.g. should initially log out "Mark can't
 // accept any new tasks right now." and "Mark would love to take on a new responsibility." if the
 // programmer is not busy.
 
-
+offerNewTask(){
+    if (this.busy = true){
+        console.log(`${this.name} is busy.`);
+ }
+ else {
+     console.log(`${this.name} would love to take on a new responsibility.`)
+ }
+}
 
 
 
@@ -73,18 +78,27 @@ function Person(name, job, age) {
 // to the programmer and list off all languages the programmer knows.
 
 
+learnLanguage(){
+    //not sure how to finish//
+}
 
-
-
+}
 // 8. Test it out - can you create different programmers and run all the methods on them? Does
 // each programmer maintain their own properties properly and independently of the other
 // programmers?
 
 
+let programmer1 = new Programmer(p1.name, p1.job, p1.age, "English");
 
+let programmer2 = new Programmer("Kelly Kapor", "HR Manager", 35);
 
+let programmer3 = new Programmer("Langston K", "Carpenter", 24);
 
+programmer1.completeTask();
+programmer1.acceptNewTask();
+programmer1.offerNewTask();
 
+console.log(programmer3);
 
 
 
@@ -94,3 +108,7 @@ function Person(name, job, age) {
 // properties to incorporate the syntax.
 // function Person(name, job, age) { }
 // function Programmer(name, job, age, languages) { }
+
+const person = (name, job,age) => {this.name: name, this.job: job, this.age: age};
+
+const programmer = (name, job, age, languages) => {this.name: name, this.job: job, this.age: age, this.language: language};
